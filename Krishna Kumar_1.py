ccd5eb1322bbfb0ee7388c1ebf5a9eab740eae16
Krishna Kumar <kkgarg2000@gmail.com>
2025-06-18 14:22:38 +0530

    Add files via upload

---
+++ Krishna Kumar_1.py
@@ -0,0 +1,15 @@
+first_number = int(input('Enter first number: '))
+second_number = int(input('Enter second number: '))
+addition = first_number + second_number
+subtraction = first_number - second_number
+multiplication = first_number * second_number
+division = first_number / second_number
+print('Addition =', addition)
+print('Subtraction =', subtraction)
+print('Multiplication =', multiplication)
+
+# Problem 2
+first_name  = input('Enter first name: ')
+second_name = input('Enter second name: ')
+print ("Hello"+" " + first_name + ' ' + second_name + '!')
+print("Thank you")
