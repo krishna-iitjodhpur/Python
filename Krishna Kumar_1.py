a05a723ce40d74f4ccb15caf688b5abf9e85ae64
Krishna Kumar <kkgarg2000@gmail.com>
2025-06-21 16:07:23 +0530

    Add files via upload

--- Krishna Kumar_1.py
+++ Krishna Kumar_1.py
@@ -1,15 +1,16 @@
-first_number = int(input('Enter first number: '))
-second_number = int(input('Enter second number: '))
-addition = first_number + second_number
-subtraction = first_number - second_number
-multiplication = first_number * second_number
-division = first_number / second_number
-print('Addition =', addition)
-print('Subtraction =', subtraction)
-print('Multiplication =', multiplication)
+# Assingment 2
+# problem 2.1
 
-# Problem 2
-first_name  = input('Enter first name: ')
-second_name = input('Enter second name: ')
-print ("Hello"+" " + first_name + ' ' + second_name + '!')
-print("Thank you")
+number1 = int(input("Enter a number: "))
+if number1 % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
+
+# problem 2.2
+total = 0
+for number2 in range(1,50):
+
+    total += number2
+print(total)
+
